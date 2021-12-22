5cba0556724e3aca67689887ea5e0635cc206242
Robby Hill <robbyhill20@yahoo.com>
2021-12-22 13:28:45 -0800

    updating handlebars and login

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3302
+      port: 3301
     }
   );
 }
--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,36 @@
+const form = document.querySelectorAll("container")
+const email = document.querySelector('#email-login').value.trim();
+const password = document.querySelector('#password-login').value.trim();
+const formInput = document.getElementsByClassName("form-input")
 const loginFormHandler = async (event) => {
-  event.preventDefault();
-
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-
+  event.preventDefault();  
   if (email && password) {
+    
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
-
+  if(!!email || !password){
+    
+  }
     if (response.ok) {
       document.location.replace('/');
     } else {
       alert('Failed to log in');
     }
   }
+
 };
+
+const enableSearch=() => {
+  if (email && password) {
+    document.getElementById("login-btn").disabled=false
+    console.log(email)
+}
+}
 
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
+formInput.addEventListener('keyup', enableSearch)
