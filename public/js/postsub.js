2d1640b07935606fa65cc0b325fdce0de7361e7b
Robby Hill <robbyhill20@yahoo.com>
2022-01-05 17:49:58 -0800

    done

--- public/js/postsub.js
+++ public/js/postsub.js
@@ -1,17 +1,14 @@
 console.log("on posting JS")
-
 const form = document.querySelector("#newuser")
 const subcard = document.querySelector("#subcard")
-const remove = document.querySelector("#remove")
+const remove = document.querySelector(".remove")
 console.log(form)
-
-form.addEventListener("submit", (e)=> { 
+form.addEventListener("submit", (e)=> {
   e.preventDefault();
   let company = form[0].value.trim()
   let cost = Number(form[1].value.trim())
   let dayofmonth = Number(form[2].value.trim())
   if(!company  || !cost || !dayofmonth) return ;
-  
   fetch('/api/sub/createsub',{
     method:'POST',
     body: JSON.stringify({company,cost,dayofmonth}),
@@ -19,18 +16,15 @@ form.addEventListener("submit", (e)=> {
   }).then((res)=>res.json())
   .then((data)=>{
     console.log(data)
-    let cardDiv = document.createElement("div")
-    cardDiv.append(subcard)
+    // let cardDiv = document.createElement("div")
+    // cardDiv.append(subcard)
+    subcard.append()
   })
-
   form[0].value = ""
   form[1].value = ""
   form[2].value = ""
-
 })
-
-function deletesub() {
-  subcard.remove()
-}
-
-remove.addEventListener("click", deletesub())
+remove.addEventListener("click", ()=>{
+  subcard.remove();
+  return false
+})
